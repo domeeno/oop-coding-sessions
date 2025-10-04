d02cad19a16f083be4ae61d1bb8a59e3e1a2f788
Dominic Flocea <45204549+domeeno@users.noreply.github.com>
2025-10-04 10:06:38 +0300

    Clarify string reference behavior

--- c-impl-of-java-string/java/Main.java
+++ c-impl-of-java-string/java/Main.java
@@ -2,11 +2,7 @@ class Main {
   public static void main(String[] args) {
     String v1 = "hello";
     String v2 = new String("hello");
-    String v3 = "Hello"; // same reference as variable v1
-    v3 = new String("Nice one");
-
-    // STRING POOL = [obj1 "v1"] 
-    // MEMORY = [obj2 "v1"]
+    String v3 = "hello"; // same reference as v1. Read more: JAVA STRING POOL
 
     if (v1 == v2) {
       System.out.println("v1 == v2: true");
